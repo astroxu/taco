57147f82e961299de3660df35f346d65b4398d63
xcg <xuchengang@uniondrug.com>
2022-01-26 14:20:27 +0800

    更新grpc调试

--- shorturl/rpc/internal/logic/shortenlogic.go
+++ shorturl/rpc/internal/logic/shortenlogic.go
@@ -27,7 +27,7 @@ func NewShortenLogic(ctx context.Context, svcCtx *svc.ServiceContext) *ShortenLo
 
 func (l *ShortenLogic) Shorten(in *transform.ShortenReq) (*transform.ShortenResp, error) {
 	// 手动代码开始，生成短链接
-	key := hash.Md5Hex([]byte(in.Url))[:6]
+	key := hash.Md5Hex([]byte(in.Url))[6:]
 	_, err := l.svcCtx.Model.Insert(&model.Shorturl{
 		Shorten: key,
 		Url:     in.Url,
