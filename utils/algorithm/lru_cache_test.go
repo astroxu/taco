156dbc5271c7b6eb2b4d1efd54e80b240d156039
sean <seanxu@gmail.com>
2022-05-16 20:40:13 +0800

    add lru cache tests

--- utils/algorithm/lru_cache_test.go
+++ utils/algorithm/lru_cache_test.go
@@ -6,9 +6,34 @@
 package algorithm
 
 import (
+	"github.com/taco/utils/internal"
 	"testing"
 )
 
 func TestLRUCache(t *testing.T) {
-	//TODO add tests
+	assert := internal.NewAssert(t, "TestLRUCache")
+	cache := NewLRUCache[int, int](2)
+
+	cache.Put(1, 1)
+	cache.Put(2, 2)
+
+	_, ok := cache.Get(0)
+	assert.Equal(false, ok)
+
+	v, ok := cache.Get(1)
+	assert.Equal(true, ok)
+	assert.Equal(1, v)
+
+	v, ok = cache.Get(2)
+	assert.Equal(true, ok)
+	assert.Equal(2, v)
+
+	cache.Put(3, 3)
+	v, ok = cache.Get(1)
+	assert.Equal(false, ok)
+	assert.NotEqual(1, v)
+
+	v, ok = cache.Get(3)
+	assert.Equal(true, ok)
+	assert.Equal(3, v)
 }
