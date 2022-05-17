bff513100d5094214f25d7bdb505d31b495e0e2c
sean <seanxu24@outlook.com>
2022-05-17 17:14:05 +0800

    test

--- utils/constraints/constraints.go
+++ utils/constraints/constraints.go
@@ -14,7 +14,7 @@ type Comparator interface {
 	Compare(v1, v2 any) int
 }
 
-// Number contains all types of number and uintptr, used for generics constraint
+// Number contains all types of number and uintptr, used for generics constraint.
 type Number interface {
 	int | int8 | int16 | int32 | int64 | uint | uint8 | uint16 | uint32 | uint64 | uintptr | float32 | float64
 }
