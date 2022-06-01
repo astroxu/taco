8b59d9ae3a3f60351dcae87b675ef3be8044e1a3
sean <seanxu24@outlook.com>
2022-06-01 09:55:06 +0800

    utils: add doubly linked list data structure

--- utils/datastructure/node.go
+++ utils/datastructure/node.go
@@ -9,7 +9,7 @@ package datastructure
 // LinkNode is a linkedlist node, which have a Value and Pre points to previous node, Next points to a next node of the link.
 type LinkNode[T any] struct {
 	Value T
-	pre   *LinkNode[T]
+	Pre   *LinkNode[T]
 	Next  *LinkNode[T]
 }
 
