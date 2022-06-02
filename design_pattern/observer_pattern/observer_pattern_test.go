37ca94868a805e1d221f0bb2eb9c72a8abdbc69c
sean <seanxu24@outlook.com>
2022-06-02 10:48:14 +0800

    design pattern: add observer pattern

---
+++ design_pattern/observer_pattern/observer_pattern_test.go
@@ -0,0 +1,31 @@
+/**
+ * @Author: Sean
+ * @Date: 2022/6/2 10:44
+ */
+
+package observer_pattern
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestObserver(t *testing.T) {
+	event := NewEvent()
+
+	investerlee := NewInvestorObserver("lee")
+	investeranne := NewInvestorObserver("anne")
+
+	share := NewShareNotifier(20.00)
+	// 注册观察者
+	share.Register(investerlee)
+	share.Register(investeranne)
+
+	share.Notify(event)
+
+	share.Remove(investerlee)
+	fmt.Println(len(share.oblist))
+	if len(share.oblist) != 1 {
+		t.Error("observer pattern error")
+	}
+}
